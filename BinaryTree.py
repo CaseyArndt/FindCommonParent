6fc9efc6d68f9c8052b0acf13fa9d12965cd1905
Casey Arndt <ca895534@gmail.com>
2023-01-26 16:38:44 -0500

    Add BinaryTree class

---
+++ BinaryTree.py
@@ -0,0 +1,9 @@
+from TreeNode import TreeNode
+
+class BinaryTree:
+    def __init__(self, items):
+        self.root = self.to_binary_tree(self, items)
+
+
+    def to_binary_tree(self, items) -> TreeNode:
+        pass
