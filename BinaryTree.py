209c8c90f7a9e2a15ddec559ce6eaf1de00f194a
Casey Arndt <ca895534@gmail.com>
2023-01-26 17:03:44 -0500

    Add Binary Tree Methods

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,9 +1,41 @@
 from TreeNode import TreeNode
 
 class BinaryTree:
-    def __init__(self, items):
-        self.root = self.to_binary_tree(self, items)
+    def __init__(self, items: list):
+        self.root = self.to_binary_tree(items)
 
 
-    def to_binary_tree(self, items) -> TreeNode:
-        pass
+    def to_binary_tree(self, items: list) -> TreeNode:
+        if len(items) == 0:
+            return None
+
+        elif len(items) == 1:
+            return TreeNode(items[0])
+
+        # Replace elements in list with TreeNodes in place, O(n) item complexity
+        for i, n in enumerate(items):
+            items[i] = TreeNode(n)
+    
+        # Construct tree in place, O(n) time complexity
+        l, r = 0, 1
+        while r < len(items):
+            if items[l].left is None:
+                items[l].l = items[r]
+            elif items[l].right is None:
+                items[l].right = items[r]
+            else:
+                l += 1
+                continue
+
+            r += 1
+
+        return items[0]
+
+    def dfs(self, root: TreeNode, val) -> TreeNode:
+        if root is None:
+            return None
+        
+        if root.val == val:
+            return root
+
+        return self.dfs(root.left, val) or self.dfs(root.right, val)
